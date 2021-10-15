562af2f1589e0128499e85799f7dfffeff6d0e85
ajid2 <ajidalmajid6@gmail.com>
2021-10-15 15:07:27 +0700

    Added options page
    Signed-off-by: ajid2 <ajidalmajid6@gmail.com>

---
+++ src/options.js
@@ -0,0 +1,26 @@
+function save_options() {
+  var url = document.getElementById('url').value;
+  var param = document.getElementById('parameter').value;
+  chrome.storage.sync.set({
+    baseUrl: url,
+    paramName: param
+  }, function() {
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+function restore_options() {
+  chrome.storage.sync.get({
+      baseUrl: '',
+      paramName: 'name'
+  }, function(items) {
+      document.getElementById('url').value = items.baseUrl;
+      document.getElementById('parameter').value = items.paramName;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
